4156ebea70e97722e6e30165ef581681e473cc13
ehmusman <ehmusman@gmail.com>
2020-12-21 22:49:15 +0500

    controlled component form value is finalized

--- src/pages/employees/EmployeeForm.js
+++ src/pages/employees/EmployeeForm.js
@@ -2,18 +2,17 @@ import { Grid, makeStyles, TextField } from '@material-ui/core'
 import React, { useState } from 'react'
 
 
-// const initialValues = {
-//     id: 0,
-//     fullName: '',
-//     email: '',
-//     mobile: '',
-//     city: '',
-//     gender: 'male',
-//     gender: 'male', // corrsponding to this property we have a group of radioButtons
-//     department: '', // corresponding to this property we have a dropdown list
-//     hireDate: new Date(),
-//     isPermanent: false // corresponding to this property we have a check box to toggle the value.
-// }
+const initialValues = {
+    id: 0,
+    fullName: '',
+    email: '',
+    mobile: '',
+    city: '',
+    gender: 'male', // corrsponding to this property we have a group of radioButtons
+    department: '', // corresponding to this property we have a dropdown list
+    hireDate: new Date(),
+    isPermanent: false // corresponding to this property we have a check box to toggle the value.
+}
 
 const useStyle = makeStyles(theme => ({
     root: {
@@ -25,39 +24,50 @@ const useStyle = makeStyles(theme => ({
 }));
 
 function EmployeeForm() {
-    const [fullName, setFullName] = useState('');
-    const [email, setEmail] = useState('');
+
     const classes = useStyle();
-    const handleNameChange = (e) => {
+    const [values, setValues] = useState(initialValues)
+    const handleOnChange = (e) => {
         e.preventDefault();
-        setFullName(e.target.value)
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
     }
-    const handleEmailChange = (e) => {
+    const handleOnSubmit = (e) => {
         e.preventDefault();
-        setEmail(e.target.value)
+        console.log(values);
+        setValues({
+            ...values,
+            fullName: '',
+            email: ''
+        })
     }
 
     return (
-        <form>
+        <form onSubmit={handleOnSubmit}>
             <Grid container className={classes.root}>
                 <Grid item xs={6}>
                     <TextField
                         variant='outlined'
                         label=" Full Name"
-                        value={fullName}
-                        onChange={handleNameChange}
+                        name='fullName'
+                        value={values.fullName}
+                        onChange={handleOnChange}
                     />
                     <TextField
                         variant='outlined'
                         label="Email"
-                        value={email}
-                        onChange={handleEmailChange}
+                        name='email'
+                        value={values.email}
+                        onChange={handleOnChange}
                     />
                 </Grid>
                 <Grid item xs={6}>
                 </Grid>
 
             </Grid>
+            <input type="submit" value="submit" />
         </form>
     )
 }
