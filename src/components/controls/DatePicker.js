2b9ea43f508819875a0b484f2242737b5d157fbb
ehmusman <ehmusman@gmail.com>
2020-12-22 16:00:23 +0500

    date picker component is created

---
+++ src/components/controls/DatePicker.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+function DatePicker() {
+    return (
+        <MuiPickersUtilsProvider>
+
+        </MuiPickersUtilsProvider>
+    )
+}
+
+export default DatePicker
