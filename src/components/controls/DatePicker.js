4b0b206e9d95e74612450c4160cfa4d41082fe86
ehmusman <ehmusman@gmail.com>
2020-12-22 16:07:58 +0500

    dateepicker component is created

--- src/components/controls/DatePicker.js
+++ src/components/controls/DatePicker.js
@@ -1,8 +1,22 @@
 import React from 'react'
-import { MuiPickersUtilsProvider } from '@material-ui/pickers'
-function DatePicker() {
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers'
+import DateFnsUtils from "@date-io/date-fns";
+function DatePicker({
+    name, label, value, onChange
+}) {
     return (
-        <MuiPickersUtilsProvider>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+                disableToolbar
+                variant='inline'
+                inputVariant='outlined'
+                label={label}
+                format='MM/dd/yyyy'
+                name={name}
+                value={value}
+                onChange={onChange}
+
+            />
 
         </MuiPickersUtilsProvider>
     )
